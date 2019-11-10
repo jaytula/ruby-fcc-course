7ccaa5c0d4dc450d59d5a42d4023d41c513a5c93
Jay T <jaytula@gmail.com>
2019-11-10 10:14:31 -0800

    Add numbers.rb

---
+++ numbers.rb
@@ -0,0 +1,15 @@
+puts 2**3
+puts 10 % 3
+puts 5.+(2)
+num = -20.487
+puts ("my fav num " + num.to_s)
+puts num.abs()
+puts num.round
+puts num.ceil
+puts num.floor()
+puts Math.sqrt(36)
+puts Math.log(1)
+puts 1 + 7
+puts 1.0 + 7
+puts 10 / 7
+puts 10 / 7.0
