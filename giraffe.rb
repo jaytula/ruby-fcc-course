e34b5c461c7a914340a3b5826bbe5d84f5e4b0b1
Jay T <jaytula@gmail.com>
2019-11-10 01:06:54 -0800

    Data Types

--- giraffe.rb
+++ giraffe.rb
@@ -1,7 +1,7 @@
-character_name = "Mike"
-character_age = "35"
-puts ("There once was a man named " + character_name)
-puts ("he was " + character_age + " years old.")
-character_name = "Tom"
-puts "He really liked the name #{character_name}"
-puts "but didn't like being #{character_age}."
+name = "Mike"
+occupation = "programmer"
+age = 75
+gpa = 3.2
+ismale = true
+istall = false
+flaws = nil
