4cf2beef6984f2d810b9e0d23f985d558f0509d9
Jay T <jaytula@gmail.com>
2019-11-10 10:33:11 -0800

    Add madlibs.rb

---
+++ madlibs.rb
@@ -0,0 +1,10 @@
+print "Enter color: "
+color = gets.chomp()
+print "Enter plural noun: "
+plural_noun = gets.chomp()
+print "Enter celebrity: "
+celebrity = gets.chomp()
+
+puts ("Roses are #{color}")
+puts ("#{plural_noun} are blue")
+puts ("I love #{celebrity}")
