e9f36eb39a15c0c298edf3374185e34ad1ab14ce
Jay T <jaytula@gmail.com>
2019-11-10 01:03:42 -0800

    Variable declarations and usage

--- giraffe.rb
+++ giraffe.rb
@@ -1,4 +1,7 @@
-puts "   /|"
-puts "  / |"
-puts " /  |"
-puts "/___|"
+character_name = "Mike"
+character_age = "35"
+puts ("There once was a man named " + character_name)
+puts ("he was " + character_age + " years old.")
+character_name = "Tom"
+puts "He really liked the name #{character_name}"
+puts "but didn't like being #{character_age}."
