681595ca06c1d6deb6ab91db7fd31d2e0892947e
Jay T <jaytula@gmail.com>
2019-11-10 10:45:44 -0800

    Add hashes.rb

---
+++ hashes.rb
@@ -0,0 +1,10 @@
+states = {
+    :Pennsylvania => "PA",
+    "Pennsylvania" => "PA2",
+    "New York" => "NY",
+    "Oregon" => "OR"
+}
+
+puts states["Oregon"] # OR
+puts states[:Pennsylvania] # PA
+puts states["Pennsylvania"]
