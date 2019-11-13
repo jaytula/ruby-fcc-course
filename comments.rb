285d9e52661389c7ba029d8ee5aafd4d0e4fb693
Jay T <jaytula@gmail.com>
2019-11-13 15:15:08 -0800

    Add comments.rb

---
+++ comments.rb
@@ -0,0 +1,15 @@
+puts "Comments are fun"  # comment after line of code
+
+# This is a comment
+
+# if true
+#     puts "Said something"
+# end
+
+=begin
+    
+This is a comment block
+
+=end
+
+word = "secret"
